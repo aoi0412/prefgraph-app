6e94b9de4c46733be04b970ce8e45a1cbc721437
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-27 19:17:31 +0900

    スイッチコンポーネントを作成

--- src/Components/Switch.tsx
+++ src/Components/Switch.tsx
@@ -1,3 +1,17 @@
-const Switch = () => <div>Enter</div>;
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { FC } from 'react';
+import './Switch.css';
+
+type Props = {
+  isOn: boolean;
+  setIsOn: (bool: boolean) => void;
+};
+const Switch: FC<Props> = ({ isOn, setIsOn }) => (
+  <div onClick={() => setIsOn(!isOn)} className={` Switch ${isOn && 'Switch--isOn'}`}>
+    <div className={` SwitchCircle ${isOn && 'SwitchCircle--isOn'}`} />
+  </div>
+);
 
 export default Switch;
