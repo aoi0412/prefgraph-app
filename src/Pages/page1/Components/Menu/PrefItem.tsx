8d09edab01d824714e7f83dc7bc838fec807f3f4
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-27 19:19:22 +0900

    メニュー内の都道府県リストの仮組みを作成

--- src/Pages/page1/Components/Menu/PrefItem.tsx
+++ src/Pages/page1/Components/Menu/PrefItem.tsx
@@ -1,3 +1,3 @@
-const PrefItem = () => <div>Enter</div>;
+const PrefItem = () => <div style={{ borderBottom: 'solid 1px gray', height: '40px', width: '100%' }}>Enter</div>;
 
 export default PrefItem;
--- src/Pages/page1/Components/Menu/PrefList.tsx
+++ src/Pages/page1/Components/Menu/PrefList.tsx
@@ -1,3 +1,11 @@
-const PrefList = () => <div>Enter</div>;
+import PrefItem from './PrefItem';
+
+const PrefList = () => (
+  <div style={{ display: 'flex', flexGrow: 1, flexDirection: 'column', borderTop: 'solid 1px gray' }}>
+    {[1, 1, 1, 1, 1].map(() => (
+      <PrefItem />
+    ))}
+  </div>
+);
 
 export default PrefList;
