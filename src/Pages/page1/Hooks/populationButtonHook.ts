548cedefc811114e7c83300efa273dfbb2723c4c
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-28 23:04:22 +0900

    人口構成のラジオボタンのカスタムフックを作成

---
+++ src/Pages/page1/Hooks/populationButtonHook.ts
@@ -0,0 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { populationType } from '../../../types';
+import { selectedPopulationTypeAtom } from '../../../Utils/recoil';
+
+export const usePopulationButton = (
+  typeName: populationType
+): [isOn: boolean, setIsOn: (typeName: populationType) => void] => {
+  const [isSelected, setIsSelected] = useState(false);
+  const [selectedPopulationType, setSelectedPopulationType] = useRecoilState(selectedPopulationTypeAtom);
+  useEffect(() => {
+    if (selectedPopulationType === typeName) {
+      setIsSelected(true);
+    } else {
+      setIsSelected(false);
+    }
+  }, [selectedPopulationType === typeName]);
+  const onSelect = (type: populationType) => {
+    setSelectedPopulationType(type);
+  };
+
+  return [isSelected, onSelect];
+};
