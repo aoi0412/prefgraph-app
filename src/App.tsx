16a901a6c6937a8f89c30fa2a39078a9b8659e62
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-28 21:40:04 +0900

    スプラッシュスクリーンを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import React from 'react';
 import './App.css';
 import { useWindowSize } from './Hooks/getWindowSize';
+import { useFetchPrefList } from './Hooks/useApiHooks';
 import MainPage from './Pages/page1/MainPage';
 
 const App = () => {
   const windowSize = useWindowSize();
+  // const isLoading = useRecoilValue(isLoadingAtom);
+  const result = useFetchPrefList();
   return (
     <div style={{ height: windowSize.height, width: windowSize.width, backgroundColor: 'white' }}>
+      <div className={`splashScreen ${result.isLoading && 'splashScreen--isOpen'}`} />
       <MainPage />
     </div>
   );
