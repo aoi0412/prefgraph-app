adfd599bd1894dd0b6f225ebbc65f6d395a9e8d3
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-28 20:51:35 +0900

    都道府県一覧を取得する関数を作成

--- src/Hooks/api.tsx
+++ src/Hooks/api.tsx
@@ -1,22 +1,54 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import axios from 'axios';
-import { prefData } from '../types';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+import { apiResult, errorResponce, prefData, resultData } from '../types';
 
-const url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
-export const fetchPrefList = async () => {
-  if (process.env.REACT_APP_RESAS_API_KEY) {
-    await axios.get(url, { headers: { 'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY } }).then((res) => {
-      if (res.status !== 200) {
-        console.log(`${res.statusText}がうまく動いていません`);
-      } else if (res.data.message) {
-        console.log(res.data.message);
-      }
-      //   return res.data.result as prefData[];
-      console.log(res.data.result as prefData[]);
-    });
-  } else {
-    alert('error');
-  }
+const url = 'https://opendata.resas-portal.go.jp/api/v1/prefecturess';
+export const useFetchPrefList = () => {
+  const [data, setData] = useState<resultData<prefData[]> | null>(null);
+  const [error, setError] = useState<errorResponce | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    if (process.env.REACT_APP_RESAS_API_KEY) {
+      axios
+        .get(url, { headers: { 'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY } })
+        .then((res: AxiosResponse<apiResult<prefData[]>>) => {
+          if (res.status !== 200) {
+            setError({
+              statusCode: `${res.status}`,
+              message: res.statusText,
+              description: 'APIがうまく動きませんでした',
+            });
+          } else if (res.data.statusCode) {
+            setError({
+              statusCode: res.data.statusCode,
+              message: res.data.message,
+              description: res.data.description ? res.data.description : '',
+            });
+          }
+          setData({
+            message: res.data.message,
+            result: res.data.result ? res.data.result : [],
+          });
+        })
+        .catch((e: AxiosError<{ error: string }>) => {
+          setError({
+            statusCode: e.code ? e.code : '',
+            message: e.message,
+            description: '',
+          });
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } else {
+      setError({
+        statusCode: '',
+        message: 'APIキーが正しくありません',
+        description: '正しいAPIキーを設定してください',
+      });
+    }
+  }, []);
+
+  return { data, error, isLoading };
 };
--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,21 @@ export type populationType = '総人口' | '年少人口' | '生産年齢人口'
 export type prefDataForGraph = {
   [title in populationType]: graphSingleData[];
 };
+
+export type apiResult<T> = {
+  message: string;
+  result?: T;
+  statusCode?: string;
+  description?: string;
+};
+
+export type errorResponce = {
+  message: string;
+  statusCode: string;
+  description: string;
+};
+
+export type resultData<T> = {
+  message: string;
+  result: T;
+};
