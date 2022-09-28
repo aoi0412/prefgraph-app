d7d1be8829eabcf8486824bca2755658650e3ebd
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-28 12:59:01 +0900

    型をまとめるファイルを追加

---
+++ src/Utils/recoil.ts
@@ -0,0 +1,17 @@
+import { atom } from 'recoil';
+import { populationType, prefData } from '../types';
+
+export const prefDataListAtom = atom({
+  key: 'prefDataListAtom',
+  default: [] as prefData[],
+});
+
+export const selectedPrefDataAtom = atom({
+  key: 'selectedDataAtom',
+  default: [] as prefData[],
+});
+
+export const selectedPopulationTypeAtom = atom({
+  key: 'selectedPopulationTypeAtom',
+  default: [] as populationType[],
+});
