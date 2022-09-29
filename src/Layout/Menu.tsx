5c02c06f53a5fdf346ae24d9b97b9a93dc9a7002
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-29 16:31:03 +0900

    メニューのバックドロップがレスポンシブになるよう修正

--- src/Layout/Menu.tsx
+++ src/Layout/Menu.tsx
@@ -2,25 +2,21 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { FC, ReactNode } from 'react';
-import { useWindowSize } from '../Hooks/getWindowSize';
-import { getDevicemode } from '../Utils/getDeviceMode';
 import './Menu.css';
 
 type Props = {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
   children: ReactNode;
+  deviceMode: 'phone' | 'tablet' | 'PC';
 };
-const Menu: FC<Props> = ({ isOpen, setIsOpen, children }) => {
-  const deviceMode = getDevicemode(useWindowSize().width);
-  return (
-    <>
-      <nav className={` DrawerMenuLeft ${deviceMode === 'PC' && 'PCmode'} ${!isOpen && 'DrawerMenuLeft--isClose'}`}>
-        {children}
-      </nav>
-      <div className={` backDrop ${isOpen && 'backDrop--isOpen'}`} onClick={() => setIsOpen(false)} />
-    </>
-  );
-};
+const Menu: FC<Props> = ({ isOpen, setIsOpen, children, deviceMode }) => (
+  <>
+    <nav className={` DrawerMenuLeft ${deviceMode === 'PC' && 'PCmode'} ${!isOpen && 'DrawerMenuLeft--isClose'}`}>
+      {children}
+    </nav>
+    <div className={` backDrop ${isOpen && 'backDrop--isOpen'}`} onClick={() => setIsOpen(false)} />
+  </>
+);
 
 export default Menu;
