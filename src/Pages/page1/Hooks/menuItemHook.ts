4912153315495e825210e69f0d9c5a8137190a76
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-09-29 16:33:18 +0900

    メニューの都道府県ボタンのカスタムフックの仕様変更に伴う修正

--- src/Pages/page1/Hooks/menuItemHook.ts
+++ src/Pages/page1/Hooks/menuItemHook.ts
@@ -1,23 +1,38 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import { prefData } from '../../../types';
-import { selectedPrefDataAtom } from '../../../Utils/recoil';
+import { useFetchPopulation } from '../../../Hooks/useApiHooks';
+import { errorResponce, populationResult, prefData, resultData } from '../../../types';
+import { prefsPopulationDataAtom, selectedPrefDataAtom } from '../../../Utils/recoil';
+import { apiToGraph } from '../../../Utils/shapeData';
 
-export const usePrefItem = (): [isSelected: boolean, onSelect: (data: prefData) => void] => {
+export const usePrefItem = (
+  data: prefData
+): { isSelected: boolean; error: errorResponce | null; onSelect: (data: prefData) => void } => {
   const [isSelected, setIsSelected] = useState(false);
   const [selectedPref, setSelectedPref] = useRecoilState(selectedPrefDataAtom);
-
-  const onSelect = (data: prefData) => {
+  const [prefsPopulationData, setPrefsPoplationData] = useRecoilState(prefsPopulationDataAtom);
+  // TODO:ここでもうAPI叩いてデータ取得しちゃおうグラフで選択された都道府県をみてAPIたたくとかやるとどれを最後に選択したとか分からなくなるので、、
+  // あと一応Recoilに取得済みの都道府県一覧を登録しておいてAPIを叩く回数を減らそう
+  const { result, error, fetchPopulation } = useFetchPopulation(data.prefCode);
+  const onSelect = () => {
     let tmp = [...selectedPref];
-    if (isSelected) {
-      tmp = tmp.filter((pref) => pref.prefCode !== data.prefCode);
+    if (tmp.find((pref) => pref.prefCode === data.prefCode)) {
+      tmp = tmp.filter((pref) => pref.prefName !== data.prefName);
     } else {
+      fetchPopulation();
       tmp.push(data);
     }
-    console.log('selectedPref is', tmp);
     setSelectedPref(tmp);
     setIsSelected(!isSelected);
   };
+  useEffect(() => {
+    if (result && result.result) {
+      const tmp = { ...prefsPopulationData };
+      tmp[data.prefName] = apiToGraph(result as resultData<populationResult>);
+      setPrefsPoplationData(tmp);
+    }
+  }, [result]);
 
-  return [isSelected, onSelect];
+  return { isSelected, error, onSelect };
 };
