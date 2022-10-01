482a916445aa51d9cb1ec13030ed02364d13264f
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-10-02 01:31:32 +0900

    inlineStyleをemotionに置換

--- src/Components/Text.tsx
+++ src/Components/Text.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable react/require-default-props */
+import { css } from '@emotion/css';
 import { FC } from 'react';
 
 type Props = {
@@ -7,16 +9,17 @@ type Props = {
   children: string;
 };
 
-const Text: FC<Props> = ({ size = 'sm', bold = false, children }) => {
-  let fontSize;
-  if (size === 'sm') {
-    fontSize = '16px';
-  } else if (size === 'md') {
-    fontSize = '20px';
-  } else {
-    fontSize = '40px';
-  }
-  return <p style={{ padding: 0, margin: 0, fontSize, fontWeight: bold ? 'bold' : 'normal' }}>{children}</p>;
+const Text: FC<Props> = ({ size = 'sm', bold = false, children }) => (
+  <p className={styles.text(size, bold)}>{children}</p>
+);
+
+const styles = {
+  text: (size: 'sm' | 'md' | 'lg', bold: boolean) => css`
+    padding: 0;
+    margin: 0;
+    font-size: ${size === 'sm' ? 16 : size === 'md' ? 20 : 40}px;
+    font-weight: ${bold ? 'bold' : 'normal'};
+  `,
 };
 
 export default Text;
--- src/Pages/page1/Components/PopulationButtonList.tsx
+++ src/Pages/page1/Components/PopulationButtonList.tsx
@@ -1,13 +1,21 @@
+import { css } from '@emotion/css';
 import { FC } from 'react';
 import { populationTypeList } from '../../../Utils/defaultDatas';
 import PopulationButton from './PopulationButton';
 
 const PopulationButtonList: FC = () => (
-  <div style={{ padding: '20px', backgroundColor: 'white' }}>
+  <div className={styles.container}>
     {populationTypeList.map((typeName) => (
       <PopulationButton typeName={typeName} />
     ))}
   </div>
 );
+
+const styles = {
+  container: css`
+    padding: 20px;
+    background-color: white;
+  `,
+};
 
 export default PopulationButtonList;
